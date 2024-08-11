195e14876e3e3ef5487a27dda5483f66a9305ccd
Carlos Esparza <ca2010es@gmail.com>
2024-08-12 00:46:24 +0200

    chane unit names

--- system-monitor-next@paradoxxx.zero.gmail.com/extension.js
+++ system-monitor-next@paradoxxx.zero.gmail.com/extension.js
@@ -18,7 +18,7 @@
 
 // Author: Florian Mounier aka paradoxxxzero
 
-import { Extension, gettext as _ } from "resource:///org/gnome/shell/extensions/extension.js";
+import { Extension, gettext} from "resource:///org/gnome/shell/extensions/extension.js";
 
 import Clutter from "gi://Clutter";
 import GLib from "gi://GLib";
@@ -40,6 +40,19 @@ import * as PopupMenu from "resource:///org/gnome/shell/ui/popupMenu.js";
 import * as Util from "resource:///org/gnome/shell/misc/util.js";
 
 import { parse_bytearray, check_sensors } from './common.js';
+
+function _(s) {
+    if (s.startsWith('KiB')) {
+        return 'KB' + s.substr(3);
+    } else if (s.startsWith('MiB')) {
+        return 'MB' + s.substr(3);
+    } else if (s.startsWith('GiB')) {
+        return 'GB' + s.substr(3);
+    } else {
+        return gettext(s);
+    }
+}
+
 
 const NetworkManager = NM;
 const UPower = UPowerGlib;
